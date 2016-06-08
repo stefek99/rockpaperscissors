fb80523483db7559e22c2036332c59b834e167b2
Michal Stefanow <mstefanow@gmail.com>
2016-06-08 21:37:42 +0100

    APOLOGIES - I realised I'm relying on angular - including it with my soluion

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,11 @@ require('../css/main.scss');
 import print from './module';
 
 print('it works well!');
+
+import angular from 'angular';
+
+var app = angular.module('app', []);
+
+app.controller("ctrl", ["$scope", function($scope) {
+  $scope.message = "it works";
+}])
