eaa58d0b7e76ba70e6b5d6d06447f8c1121aaa9c
Michal Stefanow <mstefanow@gmail.com>
2016-06-09 00:09:32 +0100

    OK, we have a very basic way of extending the app!

--- src/js/main.js
+++ src/js/main.js
@@ -6,86 +6,11 @@ print('it works well!');
 
 import angular from 'angular';
 
+import weapons from './weapons';
+
 var app = angular.module('app', []);
 
-// This is an interesting part
-// I was thinking about implementing rules in some sort of a 'GameManager'
-// Eventually decided that it is nicer if it is 'encapsulated' here
-// In that way extending the game is a matter of adding more options
-
-app.constant("gameChoices", [
-  { 
-    name: "rock",
-    img: "img/rock.jpg",
-    rules: {
-      wins: ["scissors"],
-      loses: ["paper"]
-    }
-  },
-  { 
-    name: "paper",
-    img: "img/paper.jpg",
-    rules: {
-      wins: ["rock"],
-      loses: ["scissors"]
-    }
-  },
-  { 
-    name: "scissors", 
-    img: "img/scissors.jpg",
-    rules: {
-      wins: ["paper"],
-      loses: ["rock"]
-    }
-  }
-]);
-
-// https://en.wikipedia.org/wiki/Rock-paper-scissors#Additional_weapons
-// Spock smashes scissors and vaporizes rock; he is poisoned by lizard and disproven by paper. 
-// Lizard poisons Spock and eats paper; it is crushed by rock and decapitated by scissors. 
-
-app.constant("gameChoices", [
-  { 
-    name: "rock",
-    img: "img/rock.jpg",
-    rules: {
-      wins: ["scissors", "spock"],
-      loses: ["paper", "spock"]
-    }
-  },
-  { 
-    name: "paper",
-    img: "img/paper.jpg",
-    rules: {
-      wins: ["rock", "spock"],
-      loses: ["scissors", "lizard"]
-    }
-  },
-  { 
-    name: "scissors", 
-    img: "img/scissors.jpg",
-    rules: {
-      wins: ["paper", "lizard"],
-      loses: ["rock", "spock"]
-    }
-  },
-  { 
-    name: "lizard", 
-    img: "img/lizard.jpg",
-    rules: {
-      wins: ["spock", "paper"],
-      loses: ["rock", "scissors"]
-    }
-  },
-  { 
-    name: "spock", 
-    img: "img/spock.jpg",
-    rules: {
-      wins: ["rock", "scissors"],
-      loses: ["lizard", "paper"]
-    }
-  }
-]);
+app.constant("gameChoices", weapons);
 
 app.constant("gameStates", [
   "new",
---
+++ src/js/weapons.js
@@ -0,0 +1,76 @@
+/*eslint-disable no-unused-vars*/
+
+const weaponsBASIC = [
+  { 
+    name: "rock",
+    img: "img/rock.jpg",
+    rules: {
+      wins: ["scissors"],
+      loses: ["paper"]
+    }
+  },
+  { 
+    name: "paper",
+    img: "img/paper.jpg",
+    rules: {
+      wins: ["rock"],
+      loses: ["scissors"]
+    }
+  },
+  { 
+    name: "scissors", 
+    img: "img/scissors.jpg",
+    rules: {
+      wins: ["paper"],
+      loses: ["rock"]
+    }
+  }
+];
+
+// https://en.wikipedia.org/wiki/Rock-paper-scissors#Additional_weapons
+// Spock smashes scissors and vaporizes rock; he is poisoned by lizard and disproven by paper. 
+// Lizard poisons Spock and eats paper; it is crushed by rock and decapitated by scissors. 
+const weaponsEXTENDED = [
+  { 
+    name: "rock",
+    img: "img/rock.jpg",
+    rules: {
+      wins: ["scissors", "spock"],
+      loses: ["paper", "spock"]
+    }
+  },
+  { 
+    name: "paper",
+    img: "img/paper.jpg",
+    rules: {
+      wins: ["rock", "spock"],
+      loses: ["scissors", "lizard"]
+    }
+  },
+  { 
+    name: "scissors", 
+    img: "img/scissors.jpg",
+    rules: {
+      wins: ["paper", "lizard"],
+      loses: ["rock", "spock"]
+    }
+  },
+  { 
+    name: "lizard", 
+    img: "img/lizard.jpg",
+    rules: {
+      wins: ["spock", "paper"],
+      loses: ["rock", "scissors"]
+    }
+  },
+  { 
+    name: "spock", 
+    img: "img/spock.jpg",
+    rules: {
+      wins: ["rock", "scissors"],
+      loses: ["lizard", "paper"]
+    }
+  }
+];
+
+export default weaponsBASIC;
