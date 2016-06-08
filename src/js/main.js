4d0869263fcddeec653bff0b6f3578b4f0b3b094
Michal Stefanow <mstefanow@gmail.com>
2016-06-08 23:37:53 +0100

    Adding new game options

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,83 @@ import angular from 'angular';
 
 var app = angular.module('app', []);
 
+// This is an interesting part
+// I was thinking about implementing rules in some sort of a 'GameManager'
+// Eventually decided that it is nicer if it is 'encapsulated' here
+// In that way extending the game is a matter of adding more options
+
 app.constant("gameChoices", [
-  { name: "rock",     img: "img/rock.jpg" },
-  { name: "paper",    img: "img/paper.jpg" },
-  { name: "scissors", img: "img/scissors.jpg" }
+  { 
+    name: "rock",
+    img: "img/rock.jpg",
+    rules: {
+      wins: ["scissors"],
+      loses: ["paper"]
+    }
+  },
+  { 
+    name: "paper",
+    img: "img/paper.jpg",
+    rules: {
+      wins: ["rock"],
+      loses: ["scissors"]
+    }
+  },
+  { 
+    name: "scissors", 
+    img: "img/scissors.jpg",
+    rules: {
+      wins: ["paper"],
+      loses: ["rock"]
+    }
+  }
+]);
+
+// https://en.wikipedia.org/wiki/Rock-paper-scissors#Additional_weapons
+// Spock smashes scissors and vaporizes rock; he is poisoned by lizard and disproven by paper. 
+// Lizard poisons Spock and eats paper; it is crushed by rock and decapitated by scissors. 
+
+app.constant("gameChoices", [
+  { 
+    name: "rock",
+    img: "img/rock.jpg",
+    rules: {
+      wins: ["scissors", "spock"],
+      loses: ["paper", "spock"]
+    }
+  },
+  { 
+    name: "paper",
+    img: "img/paper.jpg",
+    rules: {
+      wins: ["rock", "spock"],
+      loses: ["scissors", "lizard"]
+    }
+  },
+  { 
+    name: "scissors", 
+    img: "img/scissors.jpg",
+    rules: {
+      wins: ["paper", "lizard"],
+      loses: ["rock", "spock"]
+    }
+  },
+  { 
+    name: "lizard", 
+    img: "img/lizard.jpg",
+    rules: {
+      wins: ["spock", "paper"],
+      loses: ["rock", "scissors"]
+    }
+  },
+  { 
+    name: "spock", 
+    img: "img/spock.jpg",
+    rules: {
+      wins: ["rock", "scissors"],
+      loses: ["lizard", "paper"]
+    }
+  }
 ]);
 
 app.constant("gameStates", [
